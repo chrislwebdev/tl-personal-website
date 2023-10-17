8984138ee6ec180254336be9621178e53066a0d1
chrislwebdev <chrislwebdev@gmail.com>
2023-10-17 09:44:05 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// const portfolioLink = document.querySelector(".portfolio-redirect");
+// const aboutLink = document.querySelector(".nav-redirect");
+
+// portfolioLink.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   window.location = "subpages/portfolio.html";
+//   window.addEventListener("DOMContentLoaded", function () {
+//     if (sessionStorage.getItem("initialLoad")) {
+//       window.scrollTo(0, 500);
+//     } else {
+//       window.scrollTo(0, 0);
+//     }
+//   });
+// });
+
+// aboutLink.addEventListener("click", function (e) {
+//   window.scrollTo(0, 200);
+// });
+
+// window.addEventListener("DOMContentLoaded", function () {
+//   if (sessionStorage.getItem("initialLoad")) {
+//     window.scrollTo(0, 500);
+//   } else {
+//     window.scrollTo(0, 0);
+//   }
+// });
+
+// window.addEventListener("load", function () {
+//   sessionStorage.setItem("initialLoad", "loaded");
+// });
