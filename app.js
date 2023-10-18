5493ad9c7597d8a0062e2d71a585ca86a6b21bd2
chrislwebdev <chrislwebdev@gmail.com>
2023-10-18 21:27:40 +0300

    js push

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// window.addEventListener("DOMContentLoaded", function () {
-//   if (sessionStorage.getItem("initialLoad")) {
-//     window.scrollTo(0, 500);
-//   } else {
-//     window.scrollTo(0, 0);
-//   }
-// });
+window.addEventListener("DOMContentLoaded", function () {
+  if (sessionStorage.getItem("initialLoad")) {
+    window.scrollTo(0, 500);
+  } else {
+    window.scrollTo(0, 0);
+  }
+});
 
-// window.addEventListener("load", function () {
-//   sessionStorage.setItem("initialLoad", "loaded");
-// });
+window.addEventListener("load", function () {
+  sessionStorage.setItem("initialLoad", "loaded");
+});
 
 // const portfolioLink = document.querySelector(".portfolio-redirect");
 // const aboutLink = document.querySelector(".nav-redirect");
